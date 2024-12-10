1c221062c9b0538838dd477e4353f4eaf27bd680
julsCadenas <jscadenas12@gmail.com>
2024-12-10 15:43:15 +0800

    removing whitespace

--- summarize.py
+++ summarize.py
@@ -7,19 +7,19 @@ class Summarize:
 
     def summarize(self, text, prompt):
         inputs = self.tokenizer.encode(
-            f"{prompt}: {text}", 
-            return_tensors="pt", 
-            max_length=1024, 
+            f"{prompt}: {text}",
+            return_tensors="pt",
+            max_length=1024,
             truncation=True
         )
         summary_ids = self.model.generate(
-            inputs, 
-            max_length=130, 
-            min_length=30, 
-            length_penalty=2.0, 
-            num_beams=4, 
+            inputs,
+            max_length=130,
+            min_length=30,
+            length_penalty=2.0,
+            num_beams=4,
             # no_repeat_ngram_size=3,
-            # early_stopping=True   
+            # early_stopping=True
         )
         summary = self.tokenizer.decode(
             summary_ids[0], 
@@ -31,7 +31,7 @@ class Summarize:
         post_content = response[0]['data']['children'][0]['data'].get('selftext', '')
         comments = []
         for comment in response[1]['data']['children']:
-            if 'body' in comment['data']: 
+            if 'body' in comment['data']:
                 comments.append(comment['data']['body'])
         comments_all = ' '.join(comments)
 
