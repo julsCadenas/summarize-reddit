541c5ac3e50ed0ec989bf90b5ff241027b671054
julsCadenas <jscadenas12@gmail.com>
2025-01-31 16:59:50 +0800

    tried using json to format the output

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from summarize import Summarize
 from scrape import  Scrape
+import json
 
 def main():
     scraper = Scrape()
@@ -10,11 +11,11 @@ def main():
     post_link = 'https://www.reddit.com/r/phcareers/comments/z1ml65/best_budgetfriendly_ergonomic_chair_for_wfh/'
     response = scraper.fetch_post(post_link)
     summary = summarize.process_data(response, "Summarize and highlight popular brands")
-
-    print("Post Summary:", summary["post_summary"])
-    print("Comments Summary:", summary["comments_summary"])
+    
+    print("Post Summary:", json.dumps(summary["post_summary"], indent=4))
+    # print("Comments Summary:", json.dumps(summary["comments_summary"], indent=4))
 
 if __name__ == "__main__":
     main()
     
-# test push
+# test pushc
